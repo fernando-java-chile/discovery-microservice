178ad9889dedce7663ea45ca73b7f16215c48bf3
fernando-java-chile <fernando.java.chile@gmail.com>
2025-02-10 21:41:06 -0300

    Subiendo proyecto discovery microservice Spring Boot

---
+++ src/main/java/com/DiscoveryService/discovery_microservice/DiscoveryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.DiscoveryService.discovery_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryMicroserviceApplication.class, args);
+	}
+
+}
