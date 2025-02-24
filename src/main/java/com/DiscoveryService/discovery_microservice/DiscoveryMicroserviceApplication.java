7d0a0357d97761a51510210cb40ae9f82a049b66
fernando-java-chile <fernando.java.chile@gmail.com>
2025-02-24 16:27:07 -0300

    Creando Servidor Eureka

--- src/main/java/com/DiscoveryService/discovery_microservice/DiscoveryMicroserviceApplication.java
+++ src/main/java/com/DiscoveryService/discovery_microservice/DiscoveryMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.DiscoveryService.discovery_microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // Habilita el servidor Eureka
 public class DiscoveryMicroserviceApplication {
 
 	public static void main(String[] args) {
